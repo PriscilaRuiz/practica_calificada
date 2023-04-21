29c4bda85897bcf42d8014521eab9cf75db90405
Priscila Ruiz <priscilaruizbenavides@gmail.com>
2023-04-21 01:08:58 -0500

    Cambios5

--- insertar.php
+++ insertar.php
@@ -20,7 +20,7 @@ Al incluir el archivo "base_de_datos.php", todas sus variables están
 a nuestra disposición. Por lo que podemos acceder a ellas tal como si hubiéramos
 copiado y pegado el código
  */
-$sentencia = $base_de_datos->prepare("INSERT INTO mascotas(nombre, edad) VALUES ;");
+$sentencia = $base_de_datos->prepare("INSERT INTO mascotas(nombre, edad) VALUES (?, ?);");
 $resultado = $sentencia->execute([$nombre, $edad]); # Pasar en el mismo orden de los ?
 
 #execute regresa un booleano. True en caso de que todo vaya bien, falso en caso contrario.
